50d555beb74ed372b6037ec239e3250f24f04e34
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-02-20 14:22:47 +0100

    Version 2 script & style improved

--- src/public/scripts/utils/async_file_upload_v2.js
+++ src/public/scripts/utils/async_file_upload_v2.js
@@ -20,12 +20,28 @@ window.UTILS.async_file_upload.Uploader = class {
   lang = document.querySelector( "html" ).getAttribute( "lang" );
   url;
   input;
+  chunkSize = 1024 * 1024;
+  file;
+  start;
+  bytesLoaded;
+  chunkedUpload;
 
   constructor( element ) {
     this.element = element;
-    this.input = element.querySelector( "input" );
     this.url = "/api/" + this.lang + "/temporary_file_uploads/create_new/?format=json";
-    this.addUIhandlers();
+
+    // Setup UI handlers according to element initial state
+    if ( element.querySelector( "input[type='file']" ) ){
+      // Default state (input visible)
+      this.input = element.querySelector( "input[type='file']" );
+      this.addUIhandlers();
+    } else if ( element.querySelector( ".js--remove" ) ) {
+      // File upladed state (remove file button visible)
+      this.element.querySelector( ".js--remove" ).addEventListener( "click", this.removeButtonHandler.bind( this ) );
+    } else if ( element.querySelector( ".js--confirm" ) ) {
+      // Error state
+      this.element.querySelector( ".js--confirm" ).addEventListener( "click",  this.confirmButtonHandler.bind( this ) );
+    }
   }
 
   // Files from drag + drop
@@ -54,32 +70,104 @@ window.UTILS.async_file_upload.Uploader = class {
     if( !files ) {
       return;
     }
-    let formData = new FormData();
-    formData.append( "file", files[0] );
-    
-    let xhr = new XMLHttpRequest();
-    xhr.open( "POST", this.url, true );
+    this.file = files[0];
 
-    xhr.upload.addEventListener( "progress", this.onUploadProgress.bind( this ) );
-    xhr.addEventListener( "readystatechange", this.onReadyStateChange.bind( this) );
-    
+    if( this.file.size > 1024 * 1024 * 2 ){
+      // chunked upload
+      this.chunkedUpload = true;
+      this.start = 0;
+      this.bytesLoaded = 0;
+      // create the first chunk
+      this.createChunk( this.start );
+
+    } else {
+      // classic upload for smaler files
+      this.chunkedUpload = false;
+      let formData = new FormData();
+      formData.append( "file", files[0] );
+      
+      let xhr = new XMLHttpRequest();
+      xhr.open( "POST", this.url, true );
+      xhr.responseType = "json";
+
+      xhr.upload.addEventListener( "progress", this.onUploadProgress.bind( this ) );
+      xhr.addEventListener( "readystatechange", this.onReadyStateChange.bind( this) );
+      
+      xhr.send( formData );
+    }
+
     this.removeUIHandlers();    
     this.element.innerHTML = this.element.dataset.template_loading;
 
-    xhr.send( formData );
+  }
+
+  
+  // Prepares chunk to upload
+  createChunk( start ) {
+    // https://api.video/blog/tutorials/uploading-large-files-with-javascript/
+    // https://accreditly.io/articles/uploading-large-files-with-chunking-in-javascript
+    let end = Math.min( start + this.chunkSize , this.file.size );
+    let chunk = this.file.slice( start, end );
+    this.uploadChunk( chunk, start, end );
+  }
+
+  // Uploads chunk
+  uploadChunk( chunk, start, end ) {
+    // build Content-Range header for XHR request 
+    let range = "bytes " + start + "-" + ( end - 1 ) + "/" + this.file.size;
+    //console.log( "Range", range ); 
+
+    // Setup xhr request
+    let xhr = new XMLHttpRequest();
+    xhr.open( "POST", this.url, true );
+    xhr.responseType = "json";
+    xhr.upload.addEventListener( "progress", this.onUploadProgress.bind( this ) );
+    xhr.addEventListener( "readystatechange", this.onReadyStateChange.bind( this ) );
+    xhr.setRequestHeader( "Content-Range", range );
+    xhr.setRequestHeader( "Content-Type", this.file.type );
+    xhr.setRequestHeader( "Accept", "application/json, text/javascript, text/plain, */*" );
+    xhr.setRequestHeader( "Content-Disposition", "attachment; filename=" + "\"" + encodeURIComponent( this.file.name ) + "\"" );
+    xhr.setRequestHeader( "X-Requested-With", "XMLHttpRequest" );
+    
+    xhr.send( chunk );
   }
 
   // Updates progressbar
   onUploadProgress( e ) {
-    let progress = ( e.loaded * 100.0 / e.total ) || 100;
-    this.element.querySelector( ".progress-bar" ).style.width = progress + "%";
+    var progress;
+    if( this.chunkedUpload ) {
+      progress = ( ( this.bytesLoaded + e.loaded ) * 100 / this.file.size );
+    } else {
+      progress = ( e.loaded * 100.0 / e.total ) || 100;
+    }
+    //console.log( e.loaded, e.total, progress );
+    if( this.element.querySelector( ".progress-bar" ) ){
+      this.element.querySelector( ".progress-bar" ).style.width = progress + "%";
+    }
   }
 
   // Upload status change
   onReadyStateChange( e ) {
     let xhr = e.target;
     if ( xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 400 ) {
-      this.onUploadSuccess( xhr.response );
+
+      if( this.chunkedUpload ) {
+        // move start point for the next chunk
+        this.start += this.chunkSize;
+        //console.log( "Chunk starting at " + this.start + " uploaded" );
+        if( this.start < this.file.size ) {
+          // if this was not the last chunk create and start uploading the next chunk
+          this.bytesLoaded += this.chunkSize;
+          this.createChunk( this.start );
+
+        } else {
+          //console.log( "CHUNKED UPLOAD COMPLETE", xhr.response );
+          this.onUploadSuccess( xhr.response );
+        }
+
+      } else {
+        this.onUploadSuccess( xhr.response );
+      }
     } else if( xhr.readyState === 4 ) {
       this.onUploadError( xhr.response );
     }
@@ -88,7 +176,7 @@ window.UTILS.async_file_upload.Uploader = class {
   // Shows info about uploaded file
   onUploadSuccess( response ){
     let template = this.element.dataset.template_done;
-    response = JSON.parse( response );
+    //response = JSON.parse( response );
     template = template
       .replace( "%filename%", this.escapeHtml( response.filename ) )
       .replace( "%fileext%", this.escapeHtml( response.filename.split( "." ).pop().toLowerCase() ) )
@@ -104,7 +192,6 @@ window.UTILS.async_file_upload.Uploader = class {
   onUploadError( response ) {
     let template = this.element.dataset.template_error;
     let errMsg = "Error occurred";
-    response = JSON.parse( response );
 
     if( response && response[0] ) {
       errMsg = response[ 0 ];
@@ -154,8 +241,7 @@ window.UTILS.async_file_upload.Uploader = class {
   // click on button displayed after error
   confirmButtonHandler() {
     this.element.querySelector( ".js--confirm" ).removeEventListener( "click",  this.confirmButtonHandler.bind( this ) );
-    this.element.innerHTML = this.element.dataset.input;
-    this.addUIhandlers();
+    this.restoreInput();
   }
 
   // click on remove button
@@ -169,9 +255,15 @@ window.UTILS.async_file_upload.Uploader = class {
 
     await fetch( url, { method: "POST" } );
     this.element.querySelector( ".js--remove" ).removeEventListener( "click", this.removeButtonHandler.bind( this ) );
-    this.element.innerHTML = this.element.dataset.input;
-    this.addUIhandlers();
+    this.restoreInput();
   };
+
+  // Restore UI to default state witn file input visible.
+  restoreInput(){
+    this.element.innerHTML = this.element.dataset.input;
+    this.input = this.element.querySelector( "input[type='file']" );
+    this.addUIhandlers();
+  }
 
   escapeHtml( unsafe ) {
     return unsafe
